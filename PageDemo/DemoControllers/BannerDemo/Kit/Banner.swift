0d7d959792ce5e4205970e62f01e182c820b1ee5
吴丽娟 <wlj@zjmax.com>
2018-09-17 18:17:34 +0800

    优化布局

--- PageDemo/DemoControllers/BannerDemo/Kit/Banner.swift
+++ PageDemo/DemoControllers/BannerDemo/Kit/Banner.swift
@@ -12,9 +12,9 @@ class Banner: UIView,UIScrollViewDelegate {
     //图片地址数组
     var imgUrlArrs = [String](){
         didSet {
-            for view in self.scrollView.subviews {
-                view.removeFromSuperview()
-            }
+//            for view in self.scrollView.subviews {
+//                view.removeFromSuperview()
+//            }
             self.scrollView.contentSize.width = self.frame.size.width * CGFloat(self.imgUrlArrs.count)
             self.addImagesToScrollView()
             self.pageControl.numberOfPages = self.imgUrlArrs.count
@@ -48,14 +48,14 @@ class Banner: UIView,UIScrollViewDelegate {
         super.layoutSubviews()
         self.scrollView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
         self.scrollView.contentSize.width = self.frame.size.width * CGFloat(self.imgUrlArrs.count)
-        for view in self.scrollView.subviews {
-            view.removeFromSuperview()
+        
+        for (i,imageView) in self.scrollView.subviews.enumerated() {
+            imageView.frame = CGRect(x: CGFloat(i)*self.frame.size.width, y: 0, width: self.frame.size.width, height: self.frame.size.height)
         }
         self.pageControl.frame = CGRect(x: 100, y: 400, width: 0, height: 0)
         self.pageControl.sizeToFit()
         self.pageControl.frame.origin.x = frame.width/2-self.pageControl.frame.width/2
         self.pageControl.frame.origin.y = frame.height-self.pageControl.frame.height
-        self.addImagesToScrollView()
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -69,19 +69,16 @@ class Banner: UIView,UIScrollViewDelegate {
     }
     //添加图片到滚动视图中
     func addImagesToScrollView(){
-        if self.imgUrlArrs.count != 0 {
-            for i in 0...self.imgUrlArrs.count-1 {
-                let imageView:UIImageView = {
-                    let view = UIImageView()
-                    view.frame.size = self.frame.size
-                    return view
-                }()
-                let url = URL(string: self.imgUrlArrs[i])
-                imageView.kf.setImage(with: url)
-                imageView.contentMode = .scaleAspectFit
-                imageView.frame = CGRect(x: CGFloat(i)*self.frame.size.width, y: 0, width: self.frame.size.width, height: self.frame.size.height)
-                self.scrollView.addSubview(imageView)
-            }
+        guard self.imgUrlArrs.count > 0 else {
+            return
         }
+        for (i,image) in self.imgUrlArrs.enumerated() {
+            let imageView = UIImageView()
+            let url = URL(string: image)
+            imageView.kf.setImage(with: url)
+            imageView.contentMode = .scaleAspectFit
+            self.scrollView.addSubview(imageView)
+        }
+        
     }
 }
