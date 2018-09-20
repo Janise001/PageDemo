5f857deac5dc0ad4fee1ec740119db3ee1c8d0f6
吴丽娟 <wlj@zjmax.com>
2018-09-20 18:23:32 +0800

    修改

--- PageDemo/DemoControllers/BannerDemo/BannerViewController.swift
+++ PageDemo/DemoControllers/BannerDemo/BannerViewController.swift
@@ -10,28 +10,52 @@ import UIKit
 
 class BannerViewController: UIViewController {
 
-    var banner:Banner = Banner(frame: CGRect(x: 20, y: 100, width: 0, height: 0))
-    let textField:UITextField = {
+    var banner:Banner = Banner(frame: CGRect(x: 20, y: 70, width: 0, height: 0))
+    let addTextField:UITextField = {
         let view = UITextField()
-        view.placeholder = "请网络图片地址..."
-        view.layer.borderWidth = 1
+        view.layer.borderWidth = 0.5
         view.layer.borderColor = Color.gray.cgColor
+        view.layer.cornerRadius = 5
+        view.keyboardType = .numberPad
         view.clearButtonMode = .always
         return view
     }()
-    let submitButton:UIButton = {
+    let addTextView:UITextView  = {
+        let view = UITextView()
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = Color.gray.cgColor
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    let addButton:UIButton = {
         let button = UIButton()
-        button.setTitle("提交", for: .normal)
+        button.setTitle("添加", for: .normal)
         button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         button.setTitleColor(Color.white, for: .normal)
         button.backgroundColor = Color.red
         return button
     }()
+    let deleteButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("删除", for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        button.setTitleColor(Color.white, for: .normal)
+        button.backgroundColor = Color.red
+        return button
+    }()
+    let deleteTextField:UITextField = {
+        let view = UITextField()
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = Color.gray.cgColor
+        view.layer.cornerRadius = 5
+        view.keyboardType = .numberPad
+        view.clearButtonMode = .always
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        banner.imgUrlArrs = ["http://b-ssl.duitang.com/uploads/item/201506/23/20150623184608_kj45n.jpeg",
-                             "http://www.4gbizhi.com/uploads/allimg/150316/144Ha0M-0.jpg",
-                             "http://img3.duitang.com/uploads/item/201504/06/20150406H2227_nTYsK.thumb.700_0.jpeg"]
+        self.view.backgroundColor = Color.white
+        banner.imgUrlArrs = ["http://b-ssl.duitang.com/uploads/item/201506/23/20150623184608_kj45n.jpeg"]
         banner.frame.size.height = 400
         banner.frame.size.width = 200
         
@@ -49,9 +73,13 @@ class BannerViewController: UIViewController {
 //            }
 //        })
         self.view.addSubview(self.banner)
-        self.view.addSubview(self.textField)
-        self.view.addSubview(self.submitButton)
-        self.submitButton.addTarget(self, action: #selector(addImages), for: .touchUpInside)
+        self.view.addSubview(self.addTextField)
+        self.view.addSubview(self.addButton)
+        self.view.addSubview(self.addTextView)
+        self.view.addSubview(self.deleteButton)
+        self.view.addSubview(self.deleteTextField)
+        self.addButton.addTarget(self, action: #selector(addImages), for: .touchUpInside)
+        self.deleteButton.addTarget(self, action: #selector(deleteImage), for: .touchUpInside)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -62,15 +90,24 @@ class BannerViewController: UIViewController {
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.textField.frame = CGRect(x: 0, y: 620, width: self.view.bounds.width, height: 30)
-        self.submitButton.frame = CGRect(x: 0, y: 650, width: 0, height: 0)
-        self.submitButton.sizeToFit()
+        self.addButton.frame = CGRect(x: 0, y: 490, width: 0, height: 0)
+        self.addButton.sizeToFit()
+        self.addTextField.frame = CGRect(x: 70, y: 490, width: 50, height: 30)
+        self.addTextView.frame = CGRect(x: 70, y: 530, width: self.view.bounds.width-70, height: 60)
+        self.deleteButton.sizeToFit()
+        self.deleteButton.frame.origin = CGPoint(x: 0, y: 600)
+        self.deleteTextField.frame = CGRect(x: 70, y: 600, width: 50, height: 30)
     }
     @objc func addImages(){
-        guard let url = self.textField.text else {
+        guard let url = self.addTextView.text else {
             return
         }
-        self.banner.imgUrlArrs.insert(url, at: 0)
+        let insertNum = self.addTextField.text == "" ? 0 : Int(self.addTextField.text!)
+        self.banner.imgUrlArrs.insert(url, at: insertNum!)
+    }
+    @objc func deleteImage(){
+        let deleteNum = self.deleteTextField.text! == "" ? 0 : Int(self.deleteTextField.text!)
+        self.banner.imgUrlArrs.remove(at: deleteNum!)
     }
 
 }
